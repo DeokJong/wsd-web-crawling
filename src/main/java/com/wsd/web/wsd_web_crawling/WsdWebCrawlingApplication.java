584c32548d16063b4850114d6fdb62ec204c8751
DeokJong <novelss444@jbnu.ac.kr>
2024-11-28 16:13:58 +0900

    init

---
+++ src/main/java/com/wsd/web/wsd_web_crawling/WsdWebCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.wsd.web.wsd_web_crawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdWebCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdWebCrawlingApplication.class, args);
+	}
+
+}
