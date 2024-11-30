4830b390b0ddcfe4b655aec7343d55b65fe23509
DeokJong <novelss444@jbnu.ac.kr>
2024-11-30 15:30:30 +0900

    featrue: account repository

--- src/main/java/com/wsd/web/wsd_web_crawling/common/repository/AccountRepository.java
+++ src/main/java/com/wsd/web/wsd_web_crawling/common/repository/AccountRepository.java
@@ -6,8 +6,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wsd.web.wsd_web_crawling.common.domain.Account;
 
+/**
+ * AccountRepository는 Account 엔티티에 대한 CRUD 작업을 수행하는 인터페이스입니다.
+ * 이 인터페이스는 Spring Data JPA의 JpaRepository를 확장합니다.
+ */
 public interface AccountRepository extends JpaRepository<Account, Long> {
-  public Optional<Account> findByUsername(String username);
+    /**
+     * 주어진 사용자 이름으로 Account를 찾습니다.
+     *
+     * @param username 사용자 이름
+     * @return 사용자 이름에 해당하는 Account가 존재하면 Optional<Account>를 반환합니다.
+     */
+    public Optional<Account> findByUsername(String username);
 
-  public boolean existsByUsername(String username);
+    /**
+     * 주어진 사용자 이름으로 Account의 존재 여부를 확인합니다.
+     *
+     * @param username 사용자 이름
+     * @return 사용자 이름에 해당하는 Account가 존재하면 true, 그렇지 않으면 false를 반환합니다.
+     */
+    public boolean existsByUsername(String username);
 }
